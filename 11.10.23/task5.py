8dadb8938a563f0e4ccb882d3d162acc0ee73ee7
Timofey Nosenkov <116382351+timih18@users.noreply.github.com>
2023-10-13 16:40:40 +0300

    Add files via upload

---
+++ 11.10.23/task5.py
@@ -0,0 +1,11 @@
+s = 'abcx'
+cnt = 0
+for a in s:
+    for b in s:
+        for c in s:
+            for d in s:
+                for e in s:
+                    w = a + b + c + d + e
+                    if w.count('x') == 1:
+                        cnt += 1
+print(cnt)

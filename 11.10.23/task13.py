f12c18c3e24c6b9039e59bbbc62eabd69d0e0333
Timofey Nosenkov <116382351+timih18@users.noreply.github.com>
2023-10-14 10:23:33 +0300

    Add files via upload

---
+++ 11.10.23/task13.py
@@ -0,0 +1,7 @@
+sp = []
+for x in range(7):
+    for y in range(1, 7):
+         a = int(f'{y}{x}320', 7) + int(f'1{x}3{y}3', 9)
+         if a % 181 == 0:
+             sp.append(a)
+print(min(sp) // 181)
